9b5dd224c7584fef844fe833b8ce1e1f2a80cbf6
Wang Changjiang <wcj3570@gmail.com>
2015-05-22 21:17:27 +1000

    pull out angular commons extentions into seperate file

--- WebTest/src/main/resources/META-INF/resources/js/ellipsableTable.js
+++ WebTest/src/main/resources/META-INF/resources/js/ellipsableTable.js
@@ -1,4 +1,4 @@
-angular.module('wbApp', [])
+angular.module('wbApp', ['commons'])
 .controller('myController', ['$scope', '$interval', '$timeout', '$http', function($scope, $interval, $timeout, $http) {
 	
 	$http.get('/querySlaMissedTickets')
@@ -11,17 +11,4 @@ angular.module('wbApp', [])
 		}];
 	});
 	
-}])
-//truncate table cells with ellipsis
-//ref: http://stackoverflow.com/questions/5239758/css-truncate-table-cells-but-fit-as-much-as-possible
-.directive('ngCustEllipsable', function() {
-	return {
-		restrict : 'C',
-		transclude: true,
-		template :
-			'<div style="position:relative">' +
-			'	<div ng-transclude style="position:absolute; max-width:100%; white-space:nowrap; overflow:hidden; text-overflow:ellipsis;"></div>' +
-			'	<div ng-transclude style="display:inline-block; height:0; overflow:hidden;"></div>' +
-			'</div>'
-	};
-});
+}]);
