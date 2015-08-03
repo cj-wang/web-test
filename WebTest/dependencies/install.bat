14cfeca9fe8d7878be20e327f31a2329ff28270d
CJ Wang <wcj3570@gmail.com>
2015-08-03 15:14:52 +1000

    Combine walle-core-5.0 backend

---
+++ WebTest/dependencies/install.bat
@@ -0,0 +1,7 @@
+call mvn install:install-file -Dfile=walle-core-5.0-20141017.jar -DgroupId=cn.walle -DartifactId=walle-core -Dversion=5.0-20141017 -Dpackaging=jar
+
+call mvn install:install-file -Dfile=walle-json-1.0.jar -DgroupId=cn.walle -DartifactId=walle-json -Dversion=1.0 -Dpackaging=jar
+
+call mvn install:install-file -Dfile=ojdbc5-11.1.0.6.0.jar -DgroupId=ojdbc5 -DartifactId=ojdbc5 -Dversion=11.1.0.6.0 -Dpackaging=jar
+
+pause
