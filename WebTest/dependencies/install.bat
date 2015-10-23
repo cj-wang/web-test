f6db143a71c8daad70d2418675ecf3d7de97ade0
CJ Wang <wcj3570@gmail.com>
2015-10-23 17:06:16 +1100

    upgrade walle-core, fix conflicts with Spring 4

--- WebTest/dependencies/install.bat
+++ WebTest/dependencies/install.bat
@@ -1,4 +1,4 @@
-call mvn install:install-file -Dfile=walle-core-5.0-20141017.jar -DgroupId=cn.walle -DartifactId=walle-core -Dversion=5.0-20141017 -Dpackaging=jar
+call mvn install:install-file -Dfile=walle-core-5.0-20151023.jar -DgroupId=cn.walle -DartifactId=walle-core -Dversion=5.0-20151023 -Dpackaging=jar
 
 call mvn install:install-file -Dfile=walle-json-1.0.jar -DgroupId=cn.walle -DartifactId=walle-json -Dversion=1.0 -Dpackaging=jar
 
