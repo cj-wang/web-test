8c7c45ff88a99c87247a2e0978e6d1df571709ba
cj <cj@192.168.1.24>
2015-10-26 13:38:11 +1100

    change directive name. cache code data.

--- WebTest/src/main/resources/static/commons/angular-walle.js
+++ WebTest/src/main/resources/static/commons/angular-walle.js
@@ -2,31 +2,37 @@
 
 angular.module('walleApp', [])
 .controller('walleCtrl', ['$scope', '$interval', '$timeout', '$http', function($scope, $interval, $timeout, $http) {
-	
+	$scope.selectCodes = {};
 }])
-.directive('walleSelectcode', ['$http', function($http) {
+.directive('walleSelectCode', ['$http', function($http) {
 	return {
+		restrict : 'A',
 		scope : {},
 		link : function(scope, element, attrs) {
-			scope.loading = true;
-			scope.value = attrs.value;
-			$http.get('/selectCode/' + attrs.walleSelectcode)
-			.success(function(data, status, headers, config) {
-				scope.loading = false;
-				scope.data = data;
-			})
-			.error(function(data, status, headers, config) {
-				scope.loading = false;
-				scope.error = "Error loading data";
-			});
+			scope.attrs = attrs;
+			if (scope.$parent.selectCodes[attrs.walleSelectCode]) {
+				scope.selectCode = scope.$parent.selectCodes[attrs.walleSelectCode];
+			} else {
+				scope.selectCode = scope.$parent.selectCodes[attrs.walleSelectCode] = {};
+				scope.selectCode.loading = true;
+				$http.get('/selectCode/' + attrs.walleSelectCode)
+				.success(function(data, status, headers, config) {
+					scope.selectCode.loading = false;
+					scope.selectCode.data = data;
+				})
+				.error(function(data, status, headers, config) {
+					scope.selectCode.loading = false;
+					scope.selectCode.error = true;
+				});
+			}
 		},
 		template :
-			'<option ng-if="loading">Loading...</option>' +
-			'<option ng-if="error">Error Loading data</option>' +
-			'<option ng-repeat="dataItem in data.dataList"' +
-			'		value="{{dataItem[data.keyFieldName]}}"' +
-			'		ng-selected="dataItem[data.keyFieldName] == value">' +
-			'	{{dataItem[data.labelFieldName]}}' +
+			'<option ng-if="selectCode.loading">Loading...</option>' +
+			'<option ng-if="selectCode.error">Error Loading data</option>' +
+			'<option ng-repeat="dataItem in selectCode.data.dataList"' +
+			'		value="{{dataItem[selectCode.data.keyFieldName]}}"' +
+			'		ng-selected="dataItem[selectCode.data.keyFieldName] == attrs.value">' +
+			'	{{dataItem[selectCode.data.labelFieldName]}}' +
 			'</option>'
 	};
 }]);
