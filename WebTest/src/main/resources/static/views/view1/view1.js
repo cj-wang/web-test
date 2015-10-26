a0345a66d2cb26ec67b19a916409364aa58043ce
cj <cj@192.168.1.24>
2015-10-26 13:59:44 +1100

    use angular-walle in ng-route pages

--- WebTest/src/main/resources/static/views/view1/view1.js
+++ WebTest/src/main/resources/static/views/view1/view1.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('myApp.view1', ['ngRoute'])
+angular.module('myApp.view1', ['ngRoute', 'walleApp'])
 
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view1', {
--- WebTest/src/main/resources/static/views/view2/view2.js
+++ WebTest/src/main/resources/static/views/view2/view2.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('myApp.view2', ['ngRoute'])
+angular.module('myApp.view2', ['ngRoute', 'walleApp'])
 
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view2', {
