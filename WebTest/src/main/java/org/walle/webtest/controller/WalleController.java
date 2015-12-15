88d602e14aecd3b8660f7f60908eb05d69054b53
CJ Wang <wcj3570@gmail.com>
2015-12-15 15:07:08 +1100

    walle-typeahead done

--- WebTest/src/main/java/org/walle/webtest/controller/WalleController.java
+++ WebTest/src/main/java/org/walle/webtest/controller/WalleController.java
@@ -1,13 +1,23 @@
 package org.walle.webtest.controller;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import cn.walle.framework.common.service.SelectCodeManager;
+import cn.walle.framework.common.support.QueryField;
 import cn.walle.framework.common.support.QueryInfo;
 import cn.walle.framework.common.support.SelectCodeData;
+import cn.walle.framework.common.support.SelectCodeDefinition;
+import cn.walle.framework.core.support.PagingInfo;
 
 /**
  * REST Controller for Walle Services
@@ -25,6 +35,28 @@ public class WalleController {
 	public SelectCodeData selectCode(@PathVariable String codeType) {
 		QueryInfo queryInfo = new QueryInfo();
 		queryInfo.setQueryType(codeType);
+		return selectCodeManager.getSelectCodeData(queryInfo);
+	}
+	
+	@RequestMapping(value="/selectCode/{codeType}", params="q")
+	public SelectCodeData selectCode(@PathVariable String codeType, @RequestParam String q, @RequestParam(required=false) Integer limit) {
+		SelectCodeDefinition selectCodeDefinition = selectCodeManager.getSelectCodeDefinition(codeType);
+		Set<String> fieldNames = new HashSet<String>();
+		fieldNames.add(selectCodeDefinition.getKeyFieldName());
+		fieldNames.add(selectCodeDefinition.getLabelFieldName());
+		if (selectCodeDefinition.getColumns() != null) {
+			for (Map<String, String> column : selectCodeDefinition.getColumns()) {
+				fieldNames.add(column.get("field"));
+			}
+		}
+		
+		QueryInfo queryInfo = new QueryInfo();
+		queryInfo.setQueryType(codeType);
+		queryInfo.setQueryFields(Arrays.asList(
+				new QueryField("[" + StringUtils.collectionToCommaDelimitedString(fieldNames) + "]", "ilikeAnywhere", q)));
+		if (limit != null && limit >= 0) {
+			queryInfo.setPagingInfo(new PagingInfo(limit, 0));
+		}
 		return selectCodeManager.getSelectCodeData(queryInfo);
 	}
 	
--- WebTest/src/main/resources/static/commons/angular-walle.js
+++ WebTest/src/main/resources/static/commons/angular-walle.js
@@ -2,10 +2,6 @@
 
 angular.module('walleApp', ['ui.bootstrap'])
 
-.controller('walleCtrl', ['$scope', '$interval', '$timeout', '$http', function($scope, $interval, $timeout, $http) {
-	
-}])
-
 .directive('walleSelectCode', ['$http', function($http) {
 	return {
 		restrict : 'A',
@@ -44,30 +40,38 @@ angular.module('walleApp', ['ui.bootstrap'])
 .directive('walleTypeaheadCode', ['$compile', '$http', function($compile, $http) {
 	return {
 		restrict : 'A',
-		scope : {},
-	    terminal: true,
+		terminal : true,
+		priority : 1000,
 		link : function(scope, element, attrs) {
-			scope.attrs = attrs;
-			scope.query = function(val) {
-				return $http.get('//maps.googleapis.com/maps/api/geocode/json', {
-					params: {
-						address: val,
-						sensor: false
-					}
-				}).then(function(response){
-					return response.data.results.map(function(item){
-						return item.formatted_address;
+			if (scope.$walleSeq == undefined) {
+				scope.$walleSeq = 0;
+			}
+			var seq = scope.$walleSeq++;
+			if (! scope.$walleSelectCodeQuery) {
+				scope.$walleSelectCodeQuery = function(codeType, q) {
+					return $http.get('/selectCode/' + codeType + '?q=' + q + '&limit=' + (attrs.typeaheadLoading || 10))
+					.then(function(response){
+						return response.data.dataList.map(function(item){
+							return {
+								key: item[response.data.keyFieldName],
+								label: item[response.data.labelFieldName]
+							}
+						});
 					});
-				});
-			};
-			element.attr('uib-typeahead', 'address for address in query($viewValue)');
-			element.attr('typeahead-loading', attrs.typeaheadLoading || 'loading');
-			element.attr('typeahead-no-results', attrs.typeaheadNoResults || 'noResults');
-			element.attr('typeahead-wait-ms', attrs.typeaheadWaitMs || '500');
+				};
+			}
 			element.removeAttr('walle-typeahead-code');
+			element.attr('uib-typeahead', 'item as item.label for item in $walleSelectCodeQuery("' + attrs.walleTypeaheadCode + '", $viewValue)');
+			element.attr('typeahead-wait-ms', attrs.typeaheadWaitMs || '500');
+			if (! attrs.typeaheadLoading) {
+				element.attr('typeahead-loading', '$walleTypeaheadLoading' + seq);
+				$compile('<i ng-show="$walleTypeaheadLoading' + seq + '" class="glyphicon glyphicon-refresh pull-left"></i>')(scope).insertAfter(element);
+			}
+			if (! attrs.typeaheadNoResults) {
+				element.attr('typeahead-no-results', '$walleTypeaheadNoResults' + seq);
+				$compile('<div ng-show="$walleTypeaheadNoResults' + seq + '"> <i class="glyphicon glyphicon-remove"></i> No Results Found </div>')(scope).insertAfter(element);
+			}
 			$compile(element)(scope);
-			$compile('<i ng-show="loading" class="glyphicon glyphicon-refresh pull-left"></i>')(scope).insertAfter(element);
-			$compile('<div ng-show="noResults"> <i class="glyphicon glyphicon-remove"></i> No Results Found </div>')(scope).insertAfter(element);
 		}
 	};
 }]);
