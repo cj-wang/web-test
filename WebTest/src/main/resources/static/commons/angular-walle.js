30946dd5465a762d82bcc2874039417c78401b35
CJ Wang <wcj3570@gmail.com>
2016-02-16 11:23:32 +1100

    use controller in directive

--- WebTest/src/main/resources/static/commons/angular-walle.js
+++ WebTest/src/main/resources/static/commons/angular-walle.js
@@ -38,38 +38,36 @@ angular.module('walleApp', ['ui.bootstrap'])
 }])
 
 .directive('walleTypeaheadCode', ['$compile', '$http', function($compile, $http) {
+	var seq = 0;
 	return {
 		restrict : 'A',
 		terminal : true,
 		priority : 1000,
-		link : function(scope, element, attrs) {
-			if (scope.$walleSeq == undefined) {
-				scope.$walleSeq = 0;
-			}
-			var seq = scope.$walleSeq++;
-			if (! scope.$walleSelectCodeQuery) {
-				scope.$walleSelectCodeQuery = function(codeType, q) {
-					return $http.get('/selectCode/' + codeType + '?q=' + q + '&limit=' + (attrs.typeaheadLoading || 10))
-					.then(function(response){
-						return response.data.dataList.map(function(item){
-							return {
-								key: item[response.data.keyFieldName],
-								label: item[response.data.labelFieldName]
-							}
-						});
+		controller : ['$scope', '$element', '$attrs', function($scope, $element, $attrs) {
+			$scope.walleSelectCodeQuery = function(codeType, q) {
+				return $http.get('/selectCode/' + codeType + '?q=' + q + '&limit=' + ($attrs.typeaheadLoading || 10))
+				.then(function(response) {
+					return response.data.dataList.map(function(item) {
+						return {
+							key : item[response.data.keyFieldName],
+							label : item[response.data.labelFieldName]
+						}
 					});
-				};
-			}
+				});
+			};
+		}],
+		link : function(scope, element, attrs) {
+			seq++;
 			element.removeAttr('walle-typeahead-code');
-			element.attr('uib-typeahead', 'item as item.label for item in $walleSelectCodeQuery("' + attrs.walleTypeaheadCode + '", $viewValue)');
+			element.attr('uib-typeahead', 'item as item.label for item in walleSelectCodeQuery("' + attrs.walleTypeaheadCode + '", $viewValue)');
 			element.attr('typeahead-wait-ms', attrs.typeaheadWaitMs || '500');
 			if (! attrs.typeaheadLoading) {
-				element.attr('typeahead-loading', '$walleTypeaheadLoading' + seq);
-				$compile('<i ng-show="$walleTypeaheadLoading' + seq + '" class="glyphicon glyphicon-refresh pull-left"></i>')(scope).insertAfter(element);
+				element.attr('typeahead-loading', 'walleTypeaheadLoading' + seq);
+				$compile('<i ng-show="walleTypeaheadLoading' + seq + '" class="glyphicon glyphicon-refresh pull-left"></i>')(scope).insertAfter(element);
 			}
 			if (! attrs.typeaheadNoResults) {
-				element.attr('typeahead-no-results', '$walleTypeaheadNoResults' + seq);
-				$compile('<div ng-show="$walleTypeaheadNoResults' + seq + '"> <i class="glyphicon glyphicon-remove"></i> No Results Found </div>')(scope).insertAfter(element);
+				element.attr('typeahead-no-results', 'walleTypeaheadNoResults' + seq);
+				$compile('<div ng-show="walleTypeaheadNoResults' + seq + '"> <i class="glyphicon glyphicon-remove"></i> No Results Found </div>')(scope).insertAfter(element);
 			}
 			$compile(element)(scope);
 		}
