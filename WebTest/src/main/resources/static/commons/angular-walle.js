8e972ae3b280666e086662fde061ca1c94de2e5c
CJ Wang <wcj3570@gmail.com>
2016-02-24 10:10:20 +1100

    comments

--- WebTest/src/main/resources/static/commons/angular-walle.js
+++ WebTest/src/main/resources/static/commons/angular-walle.js
@@ -2,6 +2,7 @@
 
 angular.module('walleApp', ['ui.bootstrap'])
 
+//walle-select-code
 .directive('walleSelectCode', ['$compile', '$http', function($compile, $http) {
 	return {
 		restrict : 'A',
@@ -9,6 +10,7 @@ angular.module('walleApp', ['ui.bootstrap'])
 		priority : 1000,
 		controller : ['$scope', '$element', '$attrs', function($scope, $element, $attrs) {
 			$scope.selectCodes = $scope.selectCodes || {};
+			//query selectCodes data
 			if (! $scope.selectCodes[$attrs.walleSelectCode]) {
 				$scope.selectCodes[$attrs.walleSelectCode] = {};
 				$scope.selectCodes[$attrs.walleSelectCode].loading = true;
@@ -26,11 +28,13 @@ angular.module('walleApp', ['ui.bootstrap'])
 					$scope.selectCodes[$attrs.walleSelectCode].error = true;
 				});
 			}
+			//set default value by value attr
 			if ($attrs.value && ! $scope[$attrs.ngModel]) {
 				$scope[$attrs.ngModel] = {
 						key : $attrs.value
 				};
 			}
+			//if required, select the 1st option by default
 			if ($attrs.required && ! $scope[$attrs.ngModel]) {
 				$scope.$watch('selectCodes.' + $attrs.walleSelectCode + '.data', function(newValue, oldValue) {
 					if ($scope.selectCodes[$attrs.walleSelectCode].data) {
@@ -42,16 +46,21 @@ angular.module('walleApp', ['ui.bootstrap'])
 		link : function(scope, element, attrs) {
 			element.removeAttr('walle-select-code');
 			element.attr('ng-options', 'item as item.label for item in selectCodes.' + attrs.walleSelectCode + '.data track by item.key');
+			//if not required, add an empty option
 			if (! attrs.required) {
 				element.append('<option value=""> - - - </option>');
 			}
+			//append loading prompt
 			$compile('<div ng-show="selectCodes.' + attrs.walleSelectCode + '.loading" style="position:fixed;padding-left:10px"> <i class="glyphicon glyphicon-refresh"></i> </div>')(scope).insertAfter(element);
+			//append error prompt
 			$compile('<div ng-show="selectCodes.' + attrs.walleSelectCode + '.error" style="position:fixed;padding-left:10px"> <i class="glyphicon glyphicon-remove"></i> No Results Found </div>')(scope).insertAfter(element);
+			//compile
 			$compile(element)(scope);
 		}
 	};
 }])
 
+//walle-typeahead-code
 .directive('walleTypeaheadCode', ['$compile', '$http', function($compile, $http) {
 	var seq = 0;
 	return {
@@ -59,6 +68,7 @@ angular.module('walleApp', ['ui.bootstrap'])
 		terminal : true,
 		priority : 1000,
 		controller : ['$scope', '$element', '$attrs', function($scope, $element, $attrs) {
+			//query selectCodes data
 			$scope.walleSelectCodeQuery = function(codeType, q) {
 				return $http.get('/selectCode/' + codeType + '?q=' + q + '&limit=' + ($attrs.typeaheadLoading || 10))
 				.then(function(response) {
@@ -76,14 +86,17 @@ angular.module('walleApp', ['ui.bootstrap'])
 			element.removeAttr('walle-typeahead-code');
 			element.attr('uib-typeahead', 'item as item.label for item in walleSelectCodeQuery("' + attrs.walleTypeaheadCode + '", $viewValue)');
 			element.attr('typeahead-wait-ms', attrs.typeaheadWaitMs || '500');
+			//append loading prompt
 			if (! attrs.typeaheadLoading) {
 				element.attr('typeahead-loading', 'walleTypeaheadLoading' + seq);
 				$compile('<div ng-show="walleTypeaheadLoading' + seq + '" style="position:fixed;padding-left:10px"> <i class="glyphicon glyphicon-refresh"></i> </div>')(scope).insertAfter(element);
 			}
+			//append error prompt
 			if (! attrs.typeaheadNoResults) {
 				element.attr('typeahead-no-results', 'walleTypeaheadNoResults' + seq);
 				$compile('<div ng-show="walleTypeaheadNoResults' + seq + '" style="position:fixed;padding-left:10px"> <i class="glyphicon glyphicon-remove"></i> No Results Found </div>')(scope).insertAfter(element);
 			}
+			//compile
 			$compile(element)(scope);
 		}
 	};
