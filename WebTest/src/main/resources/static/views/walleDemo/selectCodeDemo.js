0dc179ada8a42cf4f288b4d6528f118c1de4a749
CJ Wang <wcj3570@gmail.com>
2016-02-24 11:51:44 +1100

    ...

--- WebTest/src/main/resources/static/views/walleDemo/selectCodeDemo.js
+++ WebTest/src/main/resources/static/views/walleDemo/selectCodeDemo.js
@@ -3,11 +3,14 @@
 angular.module('selectCodeDemoApp', ['walleApp'])
 .controller('selectCodeDemoCtrl', function($scope, $http) {
 
-	//set default value in controller
+	//set default value for walle-select-code in controller
 	$scope.yesno1 = {
 			key : 0
 	};
 	
-	
+	//set default value for walle-typeahead-code in controller
+	$scope.user1 = {
+			key : 'Admin'
+	};
 	
 });
