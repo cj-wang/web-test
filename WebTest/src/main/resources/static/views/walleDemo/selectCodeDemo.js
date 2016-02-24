11bdf146d5cb55467b638401c16733abb548cb27
CJ Wang <wcj3570@gmail.com>
2016-02-25 10:39:34 +1100

    set default values by ng-init

--- WebTest/src/main/resources/static/views/walleDemo/selectCodeDemo.js
+++ WebTest/src/main/resources/static/views/walleDemo/selectCodeDemo.js
@@ -2,15 +2,5 @@
 
 angular.module('selectCodeDemoApp', ['walleApp'])
 .controller('selectCodeDemoCtrl', function($scope, $http) {
-
-	//set default value for walle-select-code in controller
-	$scope.yesno1 = {
-			key : 0
-	};
-	
-	//set default value for walle-typeahead-code in controller
-	$scope.user1 = {
-			key : 'Admin'
-	};
 	
 });
