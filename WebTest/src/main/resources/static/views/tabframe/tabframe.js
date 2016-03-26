c2f3df8331928f142b32dc439758b66a801fca80
cj <cj@192.168.1.14>
2016-03-27 00:11:38 +1100

    tab frame demo

---
+++ WebTest/src/main/resources/static/views/tabframe/tabframe.js
@@ -0,0 +1,24 @@
+'use strict';
+
+angular.module('ngApp', ['ui.walle'])
+.controller('ngCtrl', function($scope, $http) {
+	
+	var seq = 0;
+	
+	$scope.tabs = {};
+	
+	$scope.addTab = function(heading, url) {
+		seq++;
+		$scope.tabs['_' + seq] = {
+			index: seq,
+			heading: heading,
+			url: url,
+			active: true
+		};
+	};
+	
+	$scope.closeTab = function(index) {
+		delete $scope.tabs['_' + index];
+	}
+	
+});
