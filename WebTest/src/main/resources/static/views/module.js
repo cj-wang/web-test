6ad14c883017fb844d1f58e6cacccd640440aeea
CJ Wang <wcj3570@gmail.com>
2016-04-15 11:30:22 +1000

    add module.js

---
+++ WebTest/src/main/resources/static/views/module.js
@@ -0,0 +1,8 @@
+'use strict';
+
+angular.module('ngApp', ['ngRoute', 'ui.walle'])
+.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+	$routeProvider.otherwise({redirectTo: 'view1'});
+	// use the HTML5 History API
+    $locationProvider.html5Mode(true);
+}]);
