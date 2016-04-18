eec8642a8b9a83dfd442967ad1650ddbcdac12cd
CJ Wang <wcj3570@gmail.com>
2016-04-18 12:17:51 +1000

    support mvc pages

--- WebTest/src/main/resources/static/app.js
+++ WebTest/src/main/resources/static/app.js
@@ -1,7 +1,12 @@
 'use strict';
 
 angular.module('ngApp', ['ngRoute', 'ui.walle']).
-config(['$locationProvider', function($locationProvider) {
+config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+	$routeProvider.otherwise({redirectTo: function(parameters, path, search) {
+		if (path.indexOf('.') >= 0) {
+			location.reload();
+		}
+	}});
 	// use the HTML5 History API
     $locationProvider.html5Mode(true);
 }]);
--- WebTest/src/main/resources/static/views/tabframe/tabs/tableDemo.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-angular.module('ngApp', ['ui.walle'])
-.controller('ngCtrl', function($scope, $http) {
-	
-});
