7c33df6665de13719434a43d8de17141f27988c3
CJ Wang <wcj3570@gmail.com>
2016-04-18 11:48:58 +1000

    don't set default route

--- WebTest/src/main/resources/static/app.js
+++ WebTest/src/main/resources/static/app.js
@@ -1,8 +1,7 @@
 'use strict';
 
 angular.module('ngApp', ['ngRoute', 'ui.walle']).
-config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
-	$routeProvider.otherwise({redirectTo: 'view1'});
+config(['$locationProvider', function($locationProvider) {
 	// use the HTML5 History API
     $locationProvider.html5Mode(true);
 }]);
