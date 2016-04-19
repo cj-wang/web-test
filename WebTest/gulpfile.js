e9fe4e38ef97552d9c449b531e44cf58bbd44070
CJ Wang <wcj3570@gmail.com>
2016-04-19 12:32:21 +1000

    frontend-maven-plugin

---
+++ WebTest/gulpfile.js
@@ -0,0 +1,23 @@
+var gulp = require('gulp')
+var concat = require('gulp-concat')
+var sourcemaps = require('gulp-sourcemaps')
+var uglify = require('gulp-uglify')
+var ngAnnotate = require('gulp-ng-annotate')
+
+gulp.task('default', function() {
+	console.log('gulp runs as expected');
+});
+
+gulp.task('build', function () {
+	gulp.src(['src/main/resources/static/views/**/*.js'])
+	.pipe(sourcemaps.init())
+	.pipe(concat('app.js'))
+	.pipe(ngAnnotate())
+	.pipe(uglify())
+	.pipe(sourcemaps.write())
+	.pipe(gulp.dest('target/classes/static'))
+});
+
+gulp.task('watch', function () {
+  gulp.watch('src/main/resources/static/views/**/*.js', ['build'])
+});
--- WebTest/src/main/resources/static/views/module.js
+++ WebTest/src/main/resources/static/views/module.js
@@ -1,8 +1,12 @@
 'use strict';
 
-angular.module('ngApp', ['ngRoute', 'ui.walle'])
-.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
-	$routeProvider.otherwise({redirectTo: 'view1'});
+angular.module('ngApp', ['ngRoute', 'ui.walle']).
+config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+	$routeProvider.otherwise({redirectTo: function(parameters, path, search) {
+		if (path.indexOf('.') >= 0) {
+			location.reload();
+		}
+	}});
 	// use the HTML5 History API
     $locationProvider.html5Mode(true);
 }]);
