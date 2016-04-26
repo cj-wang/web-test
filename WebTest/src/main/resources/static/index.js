b745df98bcf38ad6acdc189094d960e4215d8cf6
CJ Wang <wcj3570@gmail.com>
2016-04-26 14:22:19 +1000

    add dependency angularBootstrapNavTree

--- WebTest/src/main/resources/static/index.js
+++ WebTest/src/main/resources/static/index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('ngApp', ['ngRoute', 'ui.walle'])
+angular.module('ngApp', ['ngRoute', 'ui.walle', 'angularBootstrapNavTree'])
 .controller('indexCtrl', function($scope, $location) {
 	$scope.params = $location.search();
 });
