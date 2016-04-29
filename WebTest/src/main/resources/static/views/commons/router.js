775d8ff10aa74f38a3789278de16dd331ea4c605
cj <cj@192.168.1.15>
2016-04-29 22:23:02 +1000

    separate router.js from module.js

---
+++ WebTest/src/main/resources/static/views/commons/router.js
@@ -0,0 +1,37 @@
+'use strict';
+
+angular.module('ngApp')
+.config(function($stateProvider, $urlRouterProvider, $locationProvider) {
+	
+	$stateProvider
+	.state('app', {
+		url : '/?contentOnly',
+		views: {
+			header : {
+				templateUrl : function(params) {
+					if (! params.contentOnly) {
+						return '/views/index/header.html';
+					}
+				}
+			},
+			content : {
+				templateUrl : '/views/index/welcome.html'	
+			},
+			footer : {
+				templateUrl : function(params) {
+					if (! params.contentOnly) {
+						return '/views/index/footer.html';
+					}
+				}
+			}
+		}
+	});
+	
+	$urlRouterProvider.otherwise(function($injector, $location) {
+		return '/';
+	});
+	
+	// use the HTML5 History API
+    $locationProvider.html5Mode(true);
+    
+});
--- WebTest/src/main/resources/static/views/module.js
+++ WebTest/src/main/resources/static/views/module.js
@@ -1,37 +1,3 @@
 'use strict';
 
-angular.module('ngApp', ['ngAnimate', 'ngResource', 'ui.router', 'ui.walle', 'angularBootstrapNavTree'])
-.config(function($stateProvider, $urlRouterProvider, $locationProvider) {
-	
-	$stateProvider
-	.state('app', {
-		url : '/?contentOnly',
-		views: {
-			header : {
-				templateUrl : function(params) {
-					if (! params.contentOnly) {
-						return '/views/index/header.html';
-					}
-				}
-			},
-			content : {
-				templateUrl : '/views/index/welcome.html'	
-			},
-			footer : {
-				templateUrl : function(params) {
-					if (! params.contentOnly) {
-						return '/views/index/footer.html';
-					}
-				}
-			}
-		}
-	});
-	
-	$urlRouterProvider.otherwise(function($injector, $location) {
-		return '/';
-	});
-	
-	// use the HTML5 History API
-    $locationProvider.html5Mode(true);
-    
-});
+angular.module('ngApp', ['ngAnimate', 'ngResource', 'ui.router', 'ui.walle', 'angularBootstrapNavTree']);
