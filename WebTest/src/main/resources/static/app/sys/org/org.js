06dda58bf5622d468d175de7d14d3ca1e5ebde52
cj <cj@192.168.1.15>
2016-05-11 23:40:41 +1000

    not finished

--- WebTest/src/main/resources/static/app/sys/org/org.js
+++ WebTest/src/main/resources/static/app/sys/org/org.js
@@ -15,53 +15,71 @@ angular.module('ngApp')
 })
 
 .controller('orgCtrl', function($scope, WlOrganize) {
-	WlOrganize.query({}, function(orgs) {
-		$scope.orgs = orgs;
+	//query orgs via REST
+	$scope.orgs = WlOrganize.query();
+	$scope.nodes = [];
+	$scope.nodeMap = {};
+	
+	//TODO put this watcher into directive
+	var keyField = 'organizeId';
+	var parentField = 'parentOrganizeId';
+	$scope.$watchCollection('orgs', function(data) {
+		angular.forEach(data, function(item) {
+			if (! $scope.nodeMap[item[keyField]]) {
+				var node = angular.copy(item);
+				node.data = item;
+				node.children = [];
+				$scope.nodeMap[item[keyField]] = node;
+			};
+		});
+		angular.forEach($scope.nodeMap, function(node) {
+			if ($scope.nodeMap[node.data[parentField]]) {
+				$scope.nodeMap[node.data[parentField]].children.push(node);
+			} else {
+				$scope.nodes.push(node);
+			}
+		});
 	});
 	
-	$scope.orgSelect = function(branch) {
-		$state.go('app.org.detail', {
-			organizeId : branch.data.organizeId,
-			org : branch.data
-		});
+	//on selection
+	$scope.orgSelection = function(node) {
+		//clone an org from node.data
+		$scope.org = angular.copy(node.data);
+		$scope.clone = angular.copy($scope.org);
 	};
 	
 	$scope.add = function() {
-//		$state.go('app.org.detail', {
-//			organizeId : 'new',
-//			org : {
-//				parentOrganizeId : $scope.orgTreeControl.get_selected_id()
-//			}
-//		});
+		//new org
+		$scope.org = {
+				organizeId : 'new',
+				parentOrganizedId : $scope.org ? $scope.org.organizeId : undefined
+		};
 	};
-})
-
-.controller('orgDetailCtrl', function($scope, $stateParams, WlOrganize) {
-	$scope.org = angular.copy($stateParams.org);
-	if ($stateParams.organizeId == 'new') {
-		$scope.orgTreeControl.select(null);
-	} else {
-		$scope.orgTreeControl.select($stateParams.organizeId);
-	}
-
+	
+	$scope.remove = function() {
+		alert('remove');
+		return;
+		//remove org via REST
+		$scope.org.$remove(function() {
+			//remove org from orgs, tree gets updated automatically
+			$scope.orgs.splice($scope.orgs.indexOf($scope.org), 1);
+		}, function(error) {
+			alert(error.data.message);
+		});
+	};
+	
 	$scope.save = function() {
-		alert('保存成功');
-//		WlOrganize.save($scope.org, function(org) {
-//			if ($stateParams.organizeId == 'new') {
-//				$scope.orgs.push(org);
-//				$scope.orgTreeControl.select(org.organizeId);
-//			} else {
-//				angular.copy(org, $stateParams.org);
-//			}
-//			alert('保存成功');
-//		});
+		alert('save');
+		return;
+		//save org via REST
+		$scope.org.$save(function(org) {
+			//do nothing here, as org gets updated by itself and tree gets updated automatically
+		}, function(error) {
+			alert(error.data.message);
+		});
 	};
 	
 	$scope.cancel = function() {
-		if ($stateParams.organizeId == 'new') {
-			$scope.orgTreeControl.select($stateParams.org.parentOrganizeId);
-		} else {
-			$scope.org = angular.copy($stateParams.org);
-		}
+		angular.copy($scope.clone, $scope.org);
 	};
 });
