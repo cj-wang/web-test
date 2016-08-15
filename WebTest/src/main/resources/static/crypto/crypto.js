6647ee931389bf11922aa9e6ed7b6af8d6d96d67
CJ Wang <wcj3570@gmail.com>
2016-08-15 16:00:10 +1000

    .

--- WebTest/src/main/resources/static/crypto/crypto.js
+++ WebTest/src/main/resources/static/crypto/crypto.js
@@ -15,5 +15,5 @@ function crypto_decrypt(encrypted, passphrase) {
 	var iv = CryptoJS.enc.Hex.parse(CRYPTO_IV);
 	var key128Bits100Iterations = CryptoJS.PBKDF2(passphrase, salt, {keySize: 128/32, iterations: 100});
 	var decrypted = CryptoJS.AES.decrypt(encrypted, key128Bits100Iterations, {iv: iv, mode: CryptoJS.mode.CBC, padding: CryptoJS.pad.Pkcs7});
-	return decrypted.toString(CryptoJS.enc.Utf8); ;
+	return decrypted.toString(CryptoJS.enc.Utf8);
 }
