8b849354e472d541a2ada230e3741bc7627a6326
CJ Wang <wcj3570@gmail.com>
2016-08-15 15:57:22 +1000

    js decrypt

--- WebTest/src/main/resources/static/crypto/crypto.js
+++ WebTest/src/main/resources/static/crypto/crypto.js
@@ -5,8 +5,15 @@ var CRYPTO_IV = "69135769514102d0eded589ff874cacd";
 function crypto_encrypt(plain, passphrase) {
 	var salt = CryptoJS.enc.Hex.parse(CRYPTO_SALT);
 	var iv = CryptoJS.enc.Hex.parse(CRYPTO_IV);
-	var key128Bits = CryptoJS.PBKDF2(passphrase, salt, { keySize: 128/32 }); 
 	var key128Bits100Iterations = CryptoJS.PBKDF2(passphrase, salt, {keySize: 128/32, iterations: 100});
 	var encrypted = CryptoJS.AES.encrypt(plain, key128Bits100Iterations, {iv: iv, mode: CryptoJS.mode.CBC, padding: CryptoJS.pad.Pkcs7});
 	return encrypted;
+}
+
+function crypto_decrypt(encrypted, passphrase) {
+	var salt = CryptoJS.enc.Hex.parse(CRYPTO_SALT);
+	var iv = CryptoJS.enc.Hex.parse(CRYPTO_IV);
+	var key128Bits100Iterations = CryptoJS.PBKDF2(passphrase, salt, {keySize: 128/32, iterations: 100});
+	var decrypted = CryptoJS.AES.decrypt(encrypted, key128Bits100Iterations, {iv: iv, mode: CryptoJS.mode.CBC, padding: CryptoJS.pad.Pkcs7});
+	return decrypted.toString(CryptoJS.enc.Utf8); ;
 }
